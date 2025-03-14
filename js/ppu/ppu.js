acb4a868c7e349bf6bf6b4a0af6b4efe044ee4b3
joelBit <joel.rollny@gmail.com>
2025-03-14 07:09:55 +0100

    Mirror of mapper 2 is set dynamically

--- js/ppu/ppu.js
+++ js/ppu/ppu.js
@@ -225,7 +225,7 @@ class PPU {
         }
       }
 
-      if (this.cycle >= 1 && this.cycle < 258) {
+      if (this.cycle >= 1 && this.cycle < 255) {
         this.checkIfSpriteZeroHit();
       }
 
@@ -358,11 +358,8 @@ class PPU {
       if (this.maskRegister.getRenderBackgroundLeft() | this.maskRegister.getRenderSpritesLeft()) {
         this.statusRegister.setSpriteZeroHit();
       } else {
-        if (this.cycle >= 9 && this.cycle < 255) {
+        if (this.cycle >= 9) {
           this.statusRegister.setSpriteZeroHit();
-          if (this.getBackgroundPixel().getWord() !== 0x00 && this.getForegroundPixel().getWord() !== 0x00) {
-            this.statusRegister.setSpriteZeroHit();
-          }
         }
       }
     }
